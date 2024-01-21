a93a51e850c3a834d5ec7427e3c0d575983cf986
rahulraj60000 <78650157+rahulraj60000@users.noreply.github.com>
2024-01-21 18:24:59 +0530

    Delete book_management_application directory

--- book_management_application/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-#
-# from .models *
-#
-# admin.site.register()
-
-# Register your models here.
--- book_management_application/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BookManagementApplicationConfig(AppConfig):
-    name = 'book_management_application'
